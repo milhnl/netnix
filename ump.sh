83cf0c26425000c79b68e9ded8f2bac930d21e95
Michiel van den Heuvel <michielvdnheuvel@gmail.com>
2020-11-26 16:36:29 +0100

    Add basic download functionality

--- ump.sh
+++ ump.sh
@@ -5,6 +5,7 @@ set -eu
 # COMMON ----------------------------------------------------------------------
 daemon() ( exec "$@" >/dev/null 2>&1 & )
 die() { printf '%s\n' "$*" >&2; exit 1; }
+exists() { command -v "$1" >/dev/null 2>&1; }
 
 # PROVIDER: APPLE MUSIC -------------------------------------------------------
 ump_applemusic() {
@@ -42,12 +43,45 @@ mpv_command() {
     } | nc -U "$MPV_SOCKET"
 }
 
+video_lib_location() {
+    if [ -n "${UMP_VIDEO_LIBRARY:-}" ]; then
+        echo "${UMP_VIDEO_LIBRARY:-}"
+    elif exists xdg-user-dir; then
+        echo "$(xdg-user-dir MUSIC)/Videos"
+    else
+        echo "$XDG_DATA_HOME/ump/videos"
+    fi
+}
+
+ump_youtube_download() (
+    mkdir -p "$UMP_VIDEO_LIBRARY"
+    cd "$(mktemp -d)"
+    youtube-dl --default-search ytsearch \
+        --download-archive "$UMP_VIDEO_LIBRARY/.ytdl-archive" \
+        --write-info-json \
+        -o 'ytdl.%(ext)s' "$*" >&2
+    set -- "$(<ytdl.info.json jq -r .title | yt_title_clean)" \
+        "$(find . -type f | sed 's/.*\.//' | grep -E 'mkv|webm|mp4')"
+    mv "ytdl.$2" "$UMP_VIDEO_LIBRARY/$1.$2" >&2
+    mv "ytdl.info.json" "$UMP_VIDEO_LIBRARY/.$1.info.json" >&2
+    echo "$UMP_VIDEO_LIBRARY/$1.$2"
+)
+
+ump_youtube_cached() {
+    set -- "$(find "$UMP_VIDEO_LIBRARY" -not -name '.*' -a \
+        -iname "*$(for x; do printf "%s*" "$x"; done)")"
+    [ -n "$1" ] && echo "$1" || return 1
+}
+
 ump_youtube() {
     MPV_SOCKET="${MPV_SOCKET:-$XDG_CONFIG_HOME/mpv/socket}"
+    UMP_VIDEO_LIBRARY="$(video_lib_location)"
     mpv_ensure_running
     case "$1" in
-    now) shift; mpv_command loadfile "ytdl://ytsearch:$*";;
-    add) shift; mpv_command loadfile "ytdl://ytsearch:$*" append-play;;
+    now) shift; mpv_command loadfile \
+        "$(ump_youtube_cached "$@" || echo "ytdl://ytsearch:$*")";;
+    add) shift; mpv_command loadfile \
+        "$(ump_youtube_cached "$@" || ump_youtube_download "$@")" append-play;;
     toggle) mpv_command cycle pause;;
     prev) shift; mpv_command playlist_prev;;
     next) shift; mpv_command playlist_next;;
