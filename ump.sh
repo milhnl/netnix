6dd9248c29e76ec04e5366d081501a5ea8443685
Michiel van den Heuvel <michielvdnheuvel@gmail.com>
2020-11-26 16:25:41 +0100

    Make starting mpv socket user config independent

--- ump.sh
+++ ump.sh
@@ -4,14 +4,17 @@ set -eu
 
 # COMMON ----------------------------------------------------------------------
 daemon() ( exec "$@" >/dev/null 2>&1 & )
+die() { printf '%s\n' "$*" >&2; exit 1; }
 
 # PROVIDER: YOUTUBE -----------------------------------------------------------
 mpv_ensure_running() {
-    ps -Aocomm | grep -q mpv || daemon mpv --idle
+    ps -Aocomm | grep -q mpv && [ -S "$XDG_CONFIG_HOME/mpv/socket" ] \
+        || daemon mpv --idle --input-ipc-server"=~~/socket" \
+        || die "Can't start idle mpv"
+    until mpv_command get_version >/dev/null; do sleep 1; done
 }
 
 mpv_command() {
-    mpv_ensure_running
     {
         printf '{ "command": ['
         for x; do printf '"%s",' "$x"; done
@@ -20,12 +23,14 @@ mpv_command() {
 }
 
 ump_youtube() {
+    mpv_ensure_running
     case "$1" in
     now) shift; mpv_command loadfile "ytdl://ytsearch:$*";;
     add) shift; mpv_command loadfile "ytdl://ytsearch:$*" append-play;;
     toggle) mpv_command cycle pause;;
     prev) shift; mpv_command playlist_prev;;
     next) shift; mpv_command playlist_next;;
+    *) die 'Error: unsupported operation';;
     esac
 }
 
