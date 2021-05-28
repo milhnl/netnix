b2ac247701829a82fdf5c2fe5199ce4269149432
Michiel van den Heuvel <michielvdnheuvel@gmail.com>
2021-05-28 11:43:33 +0200

    Cache JSON library with curl -z

--- ump_library_jq.sh
+++ ump_library_jq.sh
@@ -10,11 +10,14 @@ jq_escape_regex() { #1: string
 }
 
 ump_include_library() { #1 root
-    curl -fs "${1%%/.ump-library.json}/.ump-library.json" \
-        | jq '.root = "'"${1%%/.ump-library.json}/"'"'
+    set -- "${1%%/.ump-library.json}"
+    set -- "$1" "${XDG_CACHE_HOME-$HOME/.cache}/ump/$(echo "$1" | sed s:/:_:g)"
+    curl -fs -o "$2" -z "$2" "$1/.ump-library.json" ||:
+    jq --arg root "$1/" '.root = $root' "$2" 2>/dev/null ||:
 }
 
 ump_library_jq() {
+    mkdir -p "${XDG_CACHE_HOME-$HOME/.cache}/ump"
     { for x in $UMP_LIBRARIES; do ump_include_library "$x"; done; } \
         | jq -rs '
             map(
