68801882bc93df8ba94ada0cbd9ba5e82e49aef1
Michiel van den Heuvel <michielvdnheuvel@gmail.com>
2022-03-11 08:29:59 +0100

    Fix type

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -25,7 +25,7 @@ interface FilmMeta {
 }
 
 type Item = {
-  meta: {} | FilmMeta | EpisodeMeta | MusicMeta;
+  meta: Record<never, never> | FilmMeta | EpisodeMeta | MusicMeta;
   path: string;
   type: ("music" | "video" | "subtitle")[];
 };
