1ae0f917f0965b6d5948e61d542833bf93381111
Michiel van den Heuvel <michielvdnheuvel@gmail.com>
2025-11-16 21:21:55 +0100

    Use useHashLocation from wouter

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -1,26 +1,13 @@
 import { h, render } from "preact";
-import { useEffect, useState } from "preact/hooks";
 import { Router } from "wouter-preact";
+import { useHashLocation } from "wouter-preact/use-hash-location";
 import { setup } from "goober";
 import { App } from "./App.tsx";
-
-const currentLocation = () => self.location.hash.replace(/^#/, "") || "/";
-const navigate = (to: string) => (self.location.hash = to);
-
-const useHashLocation = (): [string, (path: string) => void] => {
-  const [loc, setLoc] = useState(currentLocation());
-  useEffect(() => {
-    const handler = () => setLoc(currentLocation());
-    self.addEventListener("hashchange", handler);
-    return () => self.removeEventListener("hashchange", handler);
-  }, []);
-  return [loc, navigate];
-};
 
 setup(h);
 render(
   <Router hook={useHashLocation}>
     <App />
   </Router>,
-  document.body,
+  document.body
 );
